a8cb902fe3a4b97a1b160f1d01ee838549408ef3
FilipeTheAnalyst <fbalseiro@gmail.com>
2025-04-12 01:35:49 +0100

    pipeline updated with dynamic task mapping with S3 bucket connection

--- dags/weather_pipeline.py
+++ dags/weather_pipeline.py
@@ -3,13 +3,15 @@ import logging
 import requests
 import duckdb
 
-from airflow import DAG
+from airflow import DAG, XComArg
 from airflow.decorators import task, dag
 from airflow.utils.dates import days_ago
 from airflow.models import Variable
 from airflow.utils.trigger_rule import TriggerRule
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-
+from airflow.providers.amazon.aws.operators.s3 import (
+    S3DeleteObjectsOperator, S3ListOperator
+)
 
 @dag(
     dag_id="weather_pipeline_dynamic",
@@ -73,13 +75,18 @@ def weather_pipeline_dynamic():
 
         logging.info("Stored weather data in DuckDB for all cities.")
 
-    @task
-    def delete_s3_objects(s3_keys: list[str]):
-        """Delete the weather files from S3."""
-        s3 = S3Hook(aws_conn_id="aws_default")
-        bucket = Variable.get("s3_ingest_bucket")
-        s3.delete_objects(bucket_name=bucket, keys=s3_keys)
-        logging.info("Deleted weather data files from S3.")
+    # fetch the file names from the ingest S3 bucket
+    list_files_ingest_bucket = S3ListOperator(
+        task_id="list_files_ingest_bucket",
+        aws_conn_id="aws_default",
+        bucket=Variable.get("s3_ingest_bucket")
+    )
+
+    delete_s3_objects = S3DeleteObjectsOperator.partial(
+        task_id="delete_s3_objects",
+        bucket=Variable.get("s3_ingest_bucket"),
+        aws_conn_id="aws_default",
+    ).expand(keys=XComArg(list_files_ingest_bucket))
 
     @task(trigger_rule=TriggerRule.ONE_FAILED)
     def notify_failure():
@@ -91,10 +98,12 @@ def weather_pipeline_dynamic():
     weather_data = fetch_weather.expand(city=cities)
     s3_keys = upload_to_s3.expand(weather_obj=weather_data)
     stored = store_to_duckdb(s3_keys)
-    deleted = delete_s3_objects(s3_keys)
 
     # Failure handling
-    [weather_data, stored, deleted] >> notify_failure()
+    [weather_data, stored, list_files_ingest_bucket, delete_s3_objects] >> notify_failure()
+
+    # set dependencies for tasks not set by the TaskFlowAPI
+    s3_keys >> list_files_ingest_bucket >> stored >> delete_s3_objects
 
 # Instantiate the DAG
 weather_dag = weather_pipeline_dynamic()
